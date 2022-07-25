eb8cbe87f7d5f6360ded6021ddb42caae2040049
dariaceban <darya.ceban@gmail.com>
2022-07-25 16:23:50 +0300

    Add test actions for Fabric step

--- cypress/e2e/custom-made-coat-spec.cy.js
+++ cypress/e2e/custom-made-coat-spec.cy.js
@@ -21,15 +21,17 @@ describe('Custom Made Configurator', () => {
     cy.contains('Start designing').click()
     cy.get('[data-promo-id="page-journal-custom_made-fastest_tailors-cm_coat"]').click()
     cy.url().should('include', 'product=Coat')
-
-    // step 1 - Fabric
     cy
       .get('[class="close border-solid body-small size-small shape-round icon-only background-light hydrated"]')
       .click()
 
-    cy
-      .contains('Filter')
-      .click()
+    // step 1 - Fabric
+    cy.get('[data-code="FabricSection"]').should('have.class', 'active-nav visited')
+    cy.contains('Filter').click()
+    cy.get('[data-node-name="Fall/Winter"]').check({ force: true })
+    cy.get('[data-node-name="Wool Alpaca Polyamide"]').check({ force: true })
+    cy.contains('Blue Herringbone Wool Alpaca Polyamide').click()
+    cy.contains('Next').click()
 
     // step 2 - Style
 
